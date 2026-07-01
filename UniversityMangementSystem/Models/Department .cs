276d327ac2fcf88f24485a565e079af6001f0fc2
Hidaya21 <alabrihidaya@gmail.com>
2026-07-01 11:59:54 +0400

    Department class

--- UniversityMangementSystem/Models/Department .cs
+++ UniversityMangementSystem/Models/Department .cs
@@ -9,5 +9,18 @@ namespace UniversityManagementSystem.Models
 {
     public class Department
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int departmentId { get; set; }// system generated
+        [Required, MaxLength(100)]
+        public string departmentName { get; set; }// user input
+        [MaxLength(50)]
+        public string? building { get; set; }// user input
+        [Required, Range(0, double.MaxValue)]
+        public decimal budget { get; set; }// user input
+        public int? headInstructorId { get; set; }// foreign key
+        [ForeignKey("headInstructorId")]
+        public Instructor? headInstructor { get; set; }//navigation property 
+        public ICollection<Course>? courses { get; set; }//navigation property 
+
     }
 }
