6bd204e7012deac7966ed31b51dd5d33a69946a6
Hidaya21 <alabrihidaya@gmail.com>
2026-07-01 12:15:59 +0400

    Instructor class

--- UniversityMangementSystem/Models/Instructor .cs
+++ UniversityMangementSystem/Models/Instructor .cs
@@ -6,5 +6,27 @@ namespace UniversityManagementSystem.Models
 {
     public class Instructor
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int instructorId { get; set; }   // system generated
+
+        [Required, MaxLength(100)]
+        public string fullName { get; set; } // user input
+
+        [Required, MaxLength(150)]
+        public string email { get; set; }// user input
+
+        [MaxLength(20)]
+        public string? officeNumber { get; set; }// user input
+
+        [Required]
+        public DateTime hireDate { get; set; } // user input
+
+        [Required, Range(0.1, double.MaxValue)]
+        public decimal salary { get; set; }// user input
+
+        [Required, MaxLength(50)]
+        public string academicTitle { get; set; } // from list
+
+        public ICollection<Course>? courses {get; set; }// navigation property
     }
 }
