aff31941444e2e32941faa862496686de0422509
Hidaya21 <alabrihidaya@gmail.com>
2026-07-01 10:55:09 +0400

    Courses class

---
+++ UniversityManagementSystem/Models/Instructure.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using UniversityManagementSystem;
+
+namespace UniversityManagementSystem.Models
+{
+
+    public class Instructor()
+    {
+    }
+}
